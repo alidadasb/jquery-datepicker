45e0b7d5d227c2a6e95084e23b2cb4bca74e5b3d
Alidad <programmer@dorisa.com>
2014-03-01 09:32:26 -0500

    initial commit

--- grails-app/controllers/datepicker/CalenderController.groovy
+++ grails-app/controllers/datepicker/CalenderController.groovy
@@ -1,9 +1,23 @@
 package datepicker
 
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class CalenderController {
 
+    @Transactional
     def save(Date endDate) {
-        new Calender(endDate:endDate).save(flush:true,failOnError: true)
-        render "Result save in database:   " + Calender.list()
+
+        new Calender(endDate:endDate).save flush:true, failOnError: true
+
+        render "Result: "+ Calender.list()
+
     }
+
+
+
+
 }
